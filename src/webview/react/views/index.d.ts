c5edcb0132456f0a6d490b2c7e2cf163b45d1e2a
kiwamizamurai <24860100+kiwamizamurai@users.noreply.github.com>
2025-03-07 07:29:09 +0900

    feat: add TypeScript definitions for form-data and view components

---
+++ src/webview/react/views/index.d.ts
@@ -0,0 +1,19 @@
+declare module "views" {
+  // Import all view components
+  import TestCaseView from "./TestCaseView";
+  import TestView from "./TestView";
+  import SuiteView from "./SuiteView";
+  import SectionView from "./SectionView";
+  import RunView from "./RunView";
+  import ResultView from "./ResultView";
+
+  // Export all components
+  export {
+    TestCaseView,
+    TestView,
+    SuiteView,
+    SectionView,
+    RunView,
+    ResultView,
+  };
+}
